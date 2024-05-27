6d3370715638fcae8234d6d5eb41c1cce0f65167
Fach01 <peytenredburn01@gmail.com>
2024-05-27 17:15:03 +1000

    Update DoorTest.cs

--- Assets/Scripts/DoorTest.cs
+++ Assets/Scripts/DoorTest.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DoorTest : MonoBehaviour
 {
     Animation Door;
+    public GameObject player;
+    private bool hasplayed = false;
+    public float mindistance = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +18,11 @@ public class DoorTest : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("f"))
+        if (!hasplayed && Vector3.Distance(player.transform.position, transform.position) <= mindistance)
+        {
             Door.Play();
+            hasplayed = true;
+        }
+            
     }
 }
